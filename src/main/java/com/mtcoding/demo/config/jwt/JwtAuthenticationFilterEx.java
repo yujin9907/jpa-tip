353ce869eb15e50ac91841fa51253903e3a94344
yujin <heoyujin07@gmail.com>
2022-11-28 09:14:50 +0900

    jwt까지 필기

---
+++ src/main/java/com/mtcoding/demo/config/jwt/JwtAuthenticationFilterEx.java
@@ -0,0 +1,82 @@
+package com.mtcoding.demo.config.jwt;
+
+import java.io.IOException;
+
+import javax.security.sasl.AuthenticationException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mtcoding.demo.config.auth.LoginUser;
+import com.mtcoding.demo.dto.UserReqDto.LoginReqDto;
+import com.mtcoding.demo.dto.UserRespDto.LoginRespDto;
+import com.mtcoding.demo.util.CustomResponseUtil;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JwtAuthenticationFilterEx extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+        this.authenticationManager = authenticationManager;
+    }
+
+    // post의 /login
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+        log.debug("디버그 : attemptAuthentication 요청됨");
+
+        try {
+            ObjectMapper om = new ObjectMapper();
+            LoginReqDto loginReqDto = om.readValue(request.getInputStream(), LoginReqDto.class);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    loginReqDto.getUsername(),
+                    loginReqDto.getPassword());
+
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            return authentication;
+        } catch (Exception e) {
+            throw new InternalAuthenticationServiceException(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        log.debug("디버그 : successfulAuthentication 요청됨");
+        // 1. 세션에 있는 UserDetails 가져오기
+        LoginUser loginUser = (LoginUser) authResult.getPrincipal();
+
+        // 2. 세션값으로 토큰 생성
+        String jwtToken = JwtProcess.create(loginUser);
+
+        // 3. 토큰을 헤더에 담기
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
+
+        // 4. 토큰 담아서 성공 응답하기
+        LoginRespDto loginRespDto = new LoginRespDto(loginUser.getUser());
+        CustomResponseUtil.success(response, loginRespDto);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException failed) throws IOException, ServletException {
+        log.debug("디버그 : unsuccessfulAuthentication 요청됨");
+        CustomResponseUtil.fail(response, "로그인 실패");
+    }
+
+}
